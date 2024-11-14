f20d4e059de1ea8168536b2b82907240be9e8304
Ramazonbek <sattorovramazonbek19@gmail.com>
2024-11-14 15:40:57 +0500

    new

--- lesson.py
+++ lesson.py
@@ -1,7 +1,10 @@
-a= int(input("son kirit dabba : "))
-if a==1:
-    print("Salom")
-elif a==2:
-    print("Alik")
-else:
-    print("faqat 1 va 2ni kirit")
+while True:
+    a= int(input("son kirit dabba : "))
+    if a==1:
+        print("Salom")
+    elif a==2:
+        print("Alik")
+    elif a==3:
+        break
+    else:
+        print("faqat 1 va 2ni kirit")
